e1eae1c2ed79214db10ff9b83efe5a3b0cb4597a
Tai Nguyen <phattai2407@gmail.com>
2023-03-22 10:56:17 +0200

    Initial Commit

---
+++ Voice Note/Voice_NoteApp.swift
@@ -0,0 +1,17 @@
+//
+//  Voice_NoteApp.swift
+//  Voice Note
+//
+//  Created by iosdev on 22.3.2023.
+//
+
+import SwiftUI
+
+@main
+struct Voice_NoteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
