bec6722a5ce05bd17f44bf927517986916d21f45
Giao Ngo <giaongo.97@gmail.com>
2023-03-31 19:22:01 +0300

    add transcribe and stopTranscribing method

--- Voice Note/utils/SpeechRecognizer.swift
+++ Voice Note/utils/SpeechRecognizer.swift
@@ -54,6 +54,9 @@ class SpeechRecognizer: ObservableObject {
         }
     }
     
+    deinit {
+        reset()
+    }
     /**
      This displays error message handling
      */
@@ -67,7 +70,74 @@ class SpeechRecognizer: ObservableObject {
         transciptionText = "Error dictacting speech \(errorMessage)"
     }
     
+    private static func prepareEngine() throws -> (AVAudioEngine, SFSpeechAudioBufferRecognitionRequest) {
+        let audioEngine = AVAudioEngine()
+        let request = SFSpeechAudioBufferRecognitionRequest()
+        request.shouldReportPartialResults = true
+        
+        let audioSession = AVAudioSession.sharedInstance()
+        try audioSession.setCategory(.record, mode: .measurement, options: .duckOthers)
+        try audioSession.setActive(true,  options: .notifyOthersOnDeactivation)
+        let inputNode = audioEngine.inputNode
+        let recordingFormat = inputNode.outputFormat(forBus: 0)
+        inputNode.installTap(onBus: 0, bufferSize: 1024, format: recordingFormat) {(buffer: AVAudioPCMBuffer, when: AVAudioTime) in
+            request.append(buffer)
+        }
+        audioEngine.prepare()
+        try audioEngine.start()
+        return (audioEngine, request)
+    }
+    
+    /**
+     This creates a speech recognition task to transcibe speech to text. The result is writtent to public transcriptionText until stopTransribing method is called
+     */
+    func transcribe() {
+        DispatchQueue(label: "Speech Recognition Queue", qos:.background).async {
+            [weak self] in
+            guard let self = self, let recognizer = self.recognizer, recognizer.isAvailable else {
+                self?.displayError(RecognizeError.recognizerIsUnavailable)
+                return
+            }
+            do {
+                let (audioEngine, request) = try Self.prepareEngine()
+                self.audioEngine = audioEngine
+                self.request = request
+                self.task = recognizer.recognitionTask(with: request, resultHandler: self.recognitionHandler(result:error:))
+            } catch {
+                self.reset()
+                self.displayError(error)
+            }
+        }
+    }
+    
+    /**
+     This stops transcription
+     */
+    func stopTranscibing () {
+        reset()
+    }
+    
+    func reset() {
+        task?.cancel()
+        audioEngine?.stop()
+        audioEngine = nil
+        request = nil
+        task = nil
+    }
+    
+    private func recognitionHandler (result: SFSpeechRecognitionResult?, error:Error?) {
+        let receivedFinalResult = result?.isFinal ?? false
+        let receivedError = error != nil
+        if receivedFinalResult || receivedError {
+            audioEngine?.stop()
+            audioEngine?.inputNode.removeTap(onBus: 0)
+        }
+        if let result = result {
+            transciptionText = result.bestTranscription.formattedString
+        }
+    }
 }
+
 extension SFSpeechRecognizer {
     static func hasAuthorizationToRecognize() async -> Bool {
         await withCheckedContinuation({ continuation in
