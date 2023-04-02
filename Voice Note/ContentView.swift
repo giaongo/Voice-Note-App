e5d224887e56d3aaea25a7de41da68d0d77543b4
Tai Nguyen <phattai2407@gmail.com>
2023-04-02 11:33:17 +0300

    WIP: Work in Progress

--- Voice Note/ContentView.swift
+++ Voice Note/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("This will be map screen")
+            Text("Test token")
         }
         .padding()
     }
