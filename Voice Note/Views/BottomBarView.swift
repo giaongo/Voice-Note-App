57624432d9ad9a6c030cd3c866878ae2befeb205
rashid <momras201@gmail.com>
2023-04-08 15:59:54 +0300

    I put stop button, and pause button in vertical.

--- Voice Note/Views/BottomBarView.swift
+++ Voice Note/Views/BottomBarView.swift
@@ -10,10 +10,10 @@ import SwiftUI
 struct BottomBarView: View {
     @EnvironmentObject var voiceNoteViewModel: VoiceNoteViewModel
     @EnvironmentObject var speechRecognizer: SpeechRecognizer
-    @Binding var showSheet:Bool
-    
+    @Binding var showSheet: Bool
+
     let buttonColor = #colorLiteral(red: 0.1764705926, green: 0.01176470611, blue: 0.5607843399, alpha: 1)
-    
+
     var body: some View {
         VStack {
             HStack {
@@ -27,46 +27,47 @@ struct BottomBarView: View {
                             .foregroundColor(Color(buttonColor))
                     }
                 }
-                
-                Spacer()
-                
-                Button {
-                    clickAudioButton()
-                } label: {
-                    Image(systemName: "\(voiceNoteViewModel.isRecording ? "stop.fill" : "mic.fill")")
-                        .font(.system(size: 30))
-                        .foregroundColor(.white)
-                }
-                .padding(.all,20)
-                .background(
-                    Circle()
-                        .fill(Color(buttonColor))
-                )
-                .offset(y:-30)
-                Spacer()
-                
-                Button(action: {
-                    if voiceNoteViewModel.isRecordingPaused {
-                        voiceNoteViewModel.resumeRecording()
-                    } else {
-                        voiceNoteViewModel.pauseRecording()
-                    }
-                }, label: {
-                    Image(systemName: "\(voiceNoteViewModel.isRecordingPaused ? "play.fill" : "pause.fill")")
-                        .font(.system(size: 30))
-                        .foregroundColor(.white)
-                }).disabled(!voiceNoteViewModel.isRecording || !voiceNoteViewModel.isMicPressed)
-                .padding(.all,20)
-                .background(
-                    Circle()
-                        .fill(Color(buttonColor))
-                )
-                .offset(y:-30)
-                .opacity(voiceNoteViewModel.isMicPressed ? 1.0 : 0.0) // Add this line to control the opacity
 
-                
                 Spacer()
-                
+
+                VStack {
+                    Button {
+                        clickAudioButton()
+                    } label: {
+                        Image(systemName: "\(voiceNoteViewModel.isRecording ? "stop.fill" : "mic.fill")")
+                            .font(.system(size: 30))
+                            .foregroundColor(.white)
+                    }
+                    .padding(.all, 20)
+                    .background(
+                        Circle()
+                            .fill(Color(buttonColor))
+                    )
+                    .offset(y: -30)
+
+                    if voiceNoteViewModel.isMicPressed {
+                        Button(action: {
+                            if voiceNoteViewModel.isRecordingPaused {
+                                voiceNoteViewModel.resumeRecording()
+                            } else {
+                                voiceNoteViewModel.pauseRecording()
+                            }
+                        }, label: {
+                            Image(systemName: "\(voiceNoteViewModel.isRecordingPaused ? "play.fill" : "pause.fill")")
+                                .font(.system(size: 30))
+                                .foregroundColor(.white)
+                        }).disabled(!voiceNoteViewModel.isRecording)
+                            .padding(.all, 20)
+                            .background(
+                                Circle()
+                                    .fill(Color(buttonColor))
+                            )
+                            .offset(y: -30)
+                    }
+                }
+
+                Spacer()
+
                 NavigationLink {
                     RecordingListView()
                 } label: {
@@ -74,9 +75,8 @@ struct BottomBarView: View {
                         Image(systemName: "list.dash")
                             .font(.system(size: 25))
                             .foregroundColor(Color(buttonColor))
-                            .padding(.bottom,3)
+                            .padding(.bottom, 3)
                     }
-                    
                 }
                 Spacer()
             }
@@ -90,11 +90,11 @@ struct BottomBarView: View {
             .frame(height: 60)
         }
     }
-    
+
     private func clickAudioButton() {
         voiceNoteViewModel.isRecording.toggle()
         voiceNoteViewModel.isMicPressed.toggle()
-        print("Recording bool: \( voiceNoteViewModel.isRecording)")
+        print("Recording bool: \(voiceNoteViewModel.isRecording)")
         if voiceNoteViewModel.isRecording {
             withAnimation {
                 showSheet = true
@@ -102,6 +102,7 @@ struct BottomBarView: View {
             speechRecognizer.reset()
             speechRecognizer.transcriptionText = ""
             voiceNoteViewModel.startRecording()
+
         } else {
             voiceNoteViewModel.stopRecording()
             if let newestRecordUrl = voiceNoteViewModel.fileUrlList.last {
@@ -111,6 +112,7 @@ struct BottomBarView: View {
         }
     }
 }
+
 struct BottomBarView_Previews: PreviewProvider {
     static var previews: some View {
         BottomBarView(showSheet: .constant(false)).environmentObject(VoiceNoteViewModel(numberOfSample: samples)).environmentObject(SpeechRecognizer())
