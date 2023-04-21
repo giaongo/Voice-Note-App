78fc8bef00b39b6025d126bf5d544058a64b6acf
Anwar Ulhaq <mailtoanwar@gmail.com>
2023-04-21 23:45:02 +0300

    A protocol to formulate different kind of APIs

---
+++ Voice Note/Models/APIResource.swift
@@ -0,0 +1,37 @@
+// 
+// Created by Anwar Ulhaq on 21.4.2023.
+// 
+// 
+
+import Foundation
+
+/**
+Provide a shared implementation of,
+    an associated model type into which data needs to be converted (Temperature, Wind, etc.).
+    a resourceURL, for different type of weather api (TemperatureAPI, WindAPI, etc.)
+    optional methodPath, e.g. /v2 of /current etc.
+    parameters to filter or sort the data in the response;
+
+ */
+protocol APIResource {
+    associatedtype ModelType: Decodable
+    var resourceURL: String { get }
+    var methodPath: String? { get }
+    var filters: [(String, String)] { get }
+}
+
+extension APIResource {
+    var url: URL {
+        var components = URLComponents(string: resourceURL)!
+        components.queryItems = []
+        if let methodPath = methodPath {
+            components.path = methodPath
+        }
+        if !filters.isEmpty {
+            for (filterName, filterValue) in filters {
+                components.queryItems?.append(URLQueryItem(name: filterName, value: filterValue))
+            }
+        }
+        return components.url!
+    }
+}
