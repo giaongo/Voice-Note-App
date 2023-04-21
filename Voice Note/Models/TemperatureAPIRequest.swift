262820d52219221a7c8397743c69fb3cb4e0e8af
Anwar Ulhaq <mailtoanwar@gmail.com>
2023-04-22 00:17:11 +0300

    Request specific for temperature.

---
+++ Voice Note/Models/TemperatureAPIRequest.swift
@@ -0,0 +1,27 @@
+// 
+// Created by Anwar Ulhaq on 22.4.2023.
+// 
+// 
+
+import Foundation
+
+class TemperatureAPIRequest<Resource: APIResource> {
+    let resource: Resource
+
+    init(resource: Resource) {
+        self.resource = resource
+    }
+}
+
+extension TemperatureAPIRequest: NetworkRequest {
+    func decode(_ data: Data) -> [Resource.ModelType]? {
+        let decoder = JSONDecoder()
+        decoder.userInfo[CodingUserInfoKey.managedObjectContext] = CoreDataService.localStorage.getManageObjectContext()
+        let wrapper = try? decoder.decode(APIResponseWrapper<Resource.ModelType>.self, from: data)
+        return wrapper?.temperatureResponse
+    }
+
+    func execute(withCompletion completion: @escaping ([Resource.ModelType]?) -> Void) {
+        load(resource.url, withCompletion: completion)
+    }
+}
