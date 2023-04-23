33851b37f011b5dc8609ec27a85c5e567c012c33
rashid <momras201@gmail.com>
2023-04-23 20:33:44 +0300

    share option is not functioning as expected

--- Voice Note/Views/DetailView.swift
+++ Voice Note/Views/DetailView.swift
@@ -10,6 +10,7 @@ import CoreLocation
 struct DetailView: View {
     @EnvironmentObject var voiceNoteViewModel: VoiceNoteViewModel
     @Environment(\.presentationMode) var presentationMode
+    @State private var showShareSheet = false
     let textContainer = #colorLiteral(red: 0.4, green: 0.2039215686, blue: 0.4980392157, alpha: 0.2)
     private let voiceNote: VoiceNote
     
@@ -38,7 +39,7 @@ struct DetailView: View {
                     .cornerRadius(20)
                     .padding()
             }
-
+            
             RecordingCardView().padding(15)
             Text("Duration: \(voiceNote.duration)s")
             HStack {
@@ -59,13 +60,15 @@ struct DetailView: View {
                 
                 // Share button
                 DetailBtn(clickHander: {
-                    print("Share pressed")
-                }, icon: "square.and.arrow.up").disabled(true)
+                    showShareSheet = true
+                }, icon: "square.and.arrow.up")
+                .sheet(isPresented: $showShareSheet, content: {
+                    ShareSheet(activityItems: [voiceNote.text ?? ""])
+                })
             }
-
+            .presentationDetents([.medium, .large])
+            .presentationDragIndicator(.visible)
         }
-        .presentationDetents([.medium, .large])
-        .presentationDragIndicator(.visible)
     }
     
     private func toggleEditing() {
@@ -107,7 +110,19 @@ struct DetailBtn: View {
         }.padding(.horizontal,20)
     }
 }
+    struct ShareSheet: UIViewControllerRepresentable {
+        typealias UIViewControllerType = UIActivityViewController
+        var activityItems: [Any]
 
+        func makeUIViewController(context: Context) -> UIActivityViewController {
+            let controller = UIActivityViewController(activityItems: activityItems, applicationActivities: nil)
+            return controller
+        }
+
+        func updateUIViewController(_ uiViewController: UIActivityViewController, context: Context) {
+            // Nothing to update
+        }
+    }
 /*struct DetailView_Previews: PreviewProvider {
     static var previews: some View {
         DetailView(voiceNote: VoiceNote(
